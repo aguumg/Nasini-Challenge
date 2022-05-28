791563931103ae90878e3bad8ac9d7e8af86d769
Agustin Muñoz Gonzalez <aguu.mg@gmail.com>
2022-05-28 07:11:08 -0300

    Resolución
    Resolución empaquetada con pipenv

---
+++ challenge.py
@@ -0,0 +1,54 @@
+import pyRofex
+import sys
+
+symbol = sys.argv[1]
+user = sys.argv[3]
+pw = sys.argv[5]
+account = sys.argv[7]
+
+print('Iniciando sesión en Remarkets')
+while True:
+	try:
+		pyRofex.initialize(user=user,
+		password=pw,
+		account=account,
+		environment=pyRofex.Environment.REMARKET)
+
+		print('Consultando símbolo')
+		md = pyRofex.get_market_data(ticker=symbol, entries=[pyRofex.MarketDataEntry.BIDS, pyRofex.MarketDataEntry.LAST])
+
+		if md['status'] == 'ERROR':
+			print('Símbolo inválido')
+
+		elif md['status'] == 'OK':
+			md_price = md['marketData']['LA']['price']
+
+			print('Último precio operado: $' + str(md_price))
+
+			print('Consultando BID')
+
+			if str(md['marketData']['BI']) == 'None':
+				print('No hay BIDs activos')
+				print('Ingresando orden a: $' + str(75.25))
+				order = pyRofex.send_order(ticker = symbol,
+			                           side = pyRofex.Side.BUY,
+			                           size = 1,
+			                           price = 75.25,
+			                           order_type = pyRofex.OrderType.LIMIT)
+
+			else:
+				md_bid_price = md['marketData']['BI'][0]['price']
+				print('Precio de BID: $' + str(md_bid_price))
+				print('Ingresando orden a: $' + str(round(md_bid_price-0.1,5)))
+				order = pyRofex.send_order(ticker = symbol,
+			                           side = pyRofex.Side.BUY,
+			                           size = 1,
+			                           price = round(md_bid_price-0.1,5),
+			                           order_type = pyRofex.OrderType.LIMIT)
+
+		print('Cerrando sesión en Remarkets')
+		break
+
+	except Exception:
+		print("Error! Revise las credenciales ingresadas")
+		break
